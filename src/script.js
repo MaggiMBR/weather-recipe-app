49d253aa0d4cb5057f99d898df5efcaa22d19005
ebastidas <ebastidas3@gmail.com>
2022-03-19 14:42:16 -0500

    changes to fix

--- src/script.js
+++ src/script.js
@@ -1,11 +1,13 @@
+const apiKey = "f28fa454c198fbfeaef6e3f85f051b68"; // API Key is here for simplicity, but should not be inside the code, it should be stored as an Environmental Variable
+const apiBaseUrl = "https://api.openweathermap.org/data/2.5";
+
 // Current position
 function showPosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
 
-  let units = "metric";
-  let apiKey = "f28fa454c198fbfeaef6e3f85f051b68";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  const units = "metric";
+  const apiUrl = `${apiBaseUrl}/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
 
   axios.get(apiUrl).then(showTemperature);
   axios.get(apiUrl).then(showCity);
@@ -16,64 +18,63 @@ navigator.geolocation.getCurrentPosition(showPosition);
 
 function showTemperature(response) {
   // Temperature
-  let actualTemperature = Math.round(response.data.main.temp);
-  let h4 = document.querySelector("h4");
+  const actualTemperature = Math.round(response.data.main.temp);
+  const h4 = document.querySelector("h4");
   h4.innerHTML = ` ${actualTemperature}°`;
 
   // Feels like
-  let feelsLike = Math.round(response.data.main.feels_like);
-  let feels = document.querySelector("#feels-like");
+  const feelsLike = Math.round(response.data.main.feels_like);
+  const feels = document.querySelector("#feels-like");
   feels.innerHTML = `${feelsLike}°`;
 
   // Wind
-  let actualWind = Math.round(response.data.wind.speed);
-  let wind = document.querySelector("#wind");
+  const actualWind = Math.round(response.data.wind.speed);
+  const wind = document.querySelector("#wind");
   wind.innerHTML = `${actualWind}km/h`;
 
   //Humidity
-  let actualHumidity = Math.round(response.data.main.humidity);
-  let humidity = document.querySelector("#humidity");
+  const actualHumidity = Math.round(response.data.main.humidity);
+  const humidity = document.querySelector("#humidity");
   humidity.innerHTML = `${actualHumidity}%`;
 
   // Max degree
-  let actualMaxDg = Math.round(response.data.main.temp_max);
-  let maxDg = document.querySelector("#actual-max-dg");
+  const actualMaxDg = Math.round(response.data.main.temp_max);
+  const maxDg = document.querySelector("#actual-max-dg");
   maxDg.innerHTML = `${actualMaxDg}°/`;
 
   // Min degree
-  let actualMinDg = Math.round(response.data.main.temp_min);
-  let minDg = document.querySelector("#actual-min-dg");
+  const actualMinDg = Math.round(response.data.main.temp_min);
+  const minDg = document.querySelector("#actual-min-dg");
   minDg.innerHTML = `${actualMinDg}°`;
 
   // Weather description
-  let weatherType = response.data.weather[0].description;
-  let weather = document.querySelector("#weather-type");
+  const weatherType = response.data.weather[0].description;
+  const weather = document.querySelector("#weather-type");
   weather.innerHTML = weatherType;
 }
 
 // Current city-country
 function showCity(response) {
-  let city = response.data.name;
-  let country = response.data.sys.country;
+  const city = response.data.name;
+  const country = response.data.sys.country;
 
-  let cityCountry = document.querySelector("h2");
+  const cityCountry = document.querySelector("h2");
   cityCountry.innerHTML = `${city} - ${country}`;
 }
 
 // Search  button
 function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#form-text").value;
+  const city = document.querySelector("#form-text").value;
   search(city);
 }
-let formCity = document.querySelector("#city-form");
+const formCity = document.querySelector("#city-form");
 formCity.addEventListener("submit", handleSubmit);
 
 // Default city
 function search(city) {
-  let apiKey = "f28fa454c198fbfeaef6e3f85f051b68";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  const units = "metric";
+  const apiUrl = `${apiBaseUrl}/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayWeather);
 }
 
@@ -81,52 +82,52 @@ function search(city) {
 
 function displayWeather(response) {
   // city-country name
-  let city = response.data.name;
-  let country = response.data.sys.country;
+  const city = response.data.name;
+  const country = response.data.sys.country;
 
-  let cityCountry = document.querySelector("h2");
+  const cityCountry = document.querySelector("h2");
   cityCountry.innerHTML = `${city} - ${country}`;
 
   // Temperature
   celsiusTemperature = response.data.main.temp;
 
-  let actualTemperature = Math.round(celsiusTemperature);
-  let h4 = document.querySelector("h4");
+  const actualTemperature = Math.round(celsiusTemperature);
+  const h4 = document.querySelector("h4");
   h4.innerHTML = ` ${actualTemperature}°`;
 
   // Feels like
-  let feelsLike = Math.round(response.data.main.feels_like);
-  let feels = document.querySelector("#feels-like");
+  const feelsLike = Math.round(response.data.main.feels_like);
+  const feels = document.querySelector("#feels-like");
   feels.innerHTML = `${feelsLike}°`;
 
   // Wind
-  let actualWind = Math.round(response.data.wind.speed);
-  let wind = document.querySelector("#wind");
+  const actualWind = Math.round(response.data.wind.speed);
+  const wind = document.querySelector("#wind");
   wind.innerHTML = `${actualWind}km/h`;
 
   //Humidity
-  let actualHumidity = Math.round(response.data.main.humidity);
-  let humidity = document.querySelector("#humidity");
+  const actualHumidity = Math.round(response.data.main.humidity);
+  const humidity = document.querySelector("#humidity");
   humidity.innerHTML = `${actualHumidity}%`;
 
   // Max degree
-  let actualMaxDg = Math.round(response.data.main.temp_max);
-  let maxDg = document.querySelector("#actual-max-dg");
+  const actualMaxDg = Math.round(response.data.main.temp_max);
+  const maxDg = document.querySelector("#actual-max-dg");
   maxDg.innerHTML = `${actualMaxDg}°/`;
 
   // Min degree
-  let actualMinDg = Math.round(response.data.main.temp_min);
-  let minDg = document.querySelector("#actual-min-dg");
+  const actualMinDg = Math.round(response.data.main.temp_min);
+  const minDg = document.querySelector("#actual-min-dg");
   minDg.innerHTML = `${actualMinDg}°`;
 
   // Weather description
-  let weatherType = response.data.weather[0].description;
-  let weather = document.querySelector("#weather-type");
+  const weatherType = response.data.weather[0].description;
+  const weather = document.querySelector("#weather-type");
   weather.innerHTML = weatherType;
 
   // Date info
-  let h3 = document.querySelector("h3");
-  //let actualTime = document.querySelector("#actual-time");
+  const h3 = document.querySelector("h3");
+  //const actualTime = document.querySelector("#actual-time");
   h3.innerHTML = formatDate(response.data.dt * 1000);
 
   // Link to city coodinates
@@ -136,9 +137,9 @@ function displayWeather(response) {
 // Current Date
 
 function formatDate(timestamp) {
-  let now = new Date(timestamp);
+  const now = new Date(timestamp);
 
-  let days = [
+  const days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -147,8 +148,8 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
-  let curruentDay = days[now.getDay()];
-  let currentDate = now.getDate();
+  const currentDay = days[now.getDay()];
+  const currentDate = now.getDate();
 
   //Current hour
   let currentHour = now.getHours();
@@ -163,24 +164,23 @@ function formatDate(timestamp) {
   }
 
   //last update - time
-  let actualTime = document.querySelector("#last-update-time");
+  const actualTime = document.querySelector("#last-update-time");
   actualTime.innerHTML = `Last update at ${currentHour}:${currentMinutes}`;
 
-  return `Day | ${curruentDay}, ${currentDate}`;
+  return `Day | ${currentDay}, ${currentDate}`;
 }
 
 // City coordinates
 function getForcast(coordinates) {
-  let units = "metric";
-  let apiKey = "f28fa454c198fbfeaef6e3f85f051b68";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=${units}`;
+  const units = "metric";
+  const apiUrl = `${apiBaseUrl}/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=${units}`;
 
   axios.get(apiUrl).then(displayWeatherDay);
 }
 
 // Date city weather
 function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
+  const date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = ["SUN.", "MON.", "TUE.", "WED.", "THU.", "FRI.", "SAT."];
   return days[day];
@@ -188,9 +188,9 @@ function formatDay(timestamp) {
 
 // City Weather per day
 function displayWeatherDay(response) {
-  let dayInfoWeather = response.data.daily;
+  const dayInfoWeather = response.data.daily;
 
-  let dayElement = document.querySelector("#weatherDay");
+  const dayElement = document.querySelector("#weatherDay");
 
   let dayHTML = `<div class="row box-day-degree">`;
 
@@ -222,6 +222,6 @@ function displayWeatherDay(response) {
   dayElement.innerHTML = dayHTML;
 }
 
-let celsiusTemperature = null;
+const celsiusTemperature = null;
 
 search("Quito");
